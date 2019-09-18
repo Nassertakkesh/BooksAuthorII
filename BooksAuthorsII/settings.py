025fe779dd0acda424e7697cd41e8e2ffd3b223b
Nassertakkesh <31399192+Nassertakkesh@users.noreply.github.com>
2019-09-17 20:18:14 -0700

    Update settings.py

--- BooksAuthorsII/settings.py
+++ BooksAuthorsII/settings.py
@@ -19,10 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&%g+dj7jz)n@6pl4(_7gd2i8m+r45=a1u=12ekwui@sil0--h!'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
